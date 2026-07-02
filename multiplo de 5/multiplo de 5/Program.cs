fdb7cc2b4b6ea319b93bdcc2c2c041355e312dd1
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:17:21 -0400

    Add files via upload

---
+++ multiplo de 5/multiplo de 5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ingrese numero");
+
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 5 == 0)
+{
+    Console.WriteLine("El numero es multiplo de 5");
+}
+else
+{
+    Console.WriteLine("El numero no es multiplo de 5");
+}
