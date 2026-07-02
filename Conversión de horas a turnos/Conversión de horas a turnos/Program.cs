25ffcb53070d10b7acbf94002dc9d7931cb8432a
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:25:56 -0400

    Add files via upload

---
+++ Conversión de horas a turnos/Conversión de horas a turnos/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ingrese sus horas de turnos de 0-23");
+int turnos = int.Parse(Console.ReadLine());
+
+string clasificacion = turnos switch
+{
+    >= 6 and < 11 => "Turno de la mañana",
+    >= 12 and < 17 => "Turno de la tarde",
+    >= 18 and < 23 => "Turno de la noche",
+    >= 0 and < 5 => "Turno de la madrugada",
+};
+Console.WriteLine(clasificacion);
