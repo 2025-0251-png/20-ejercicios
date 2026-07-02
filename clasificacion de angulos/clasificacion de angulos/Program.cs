9f9b38a0ea8f9be89e05b5cdb8f233f08a53bc86
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:07:22 -0400

    Add files via upload

---
+++ clasificacion de angulos/clasificacion de angulos/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("ingrese el valor del angulo en grados");
+int angulo = int.Parse(Console.ReadLine());
+
+string clasificacion = angulo switch
+{
+    < 0 or >180 => "angulo invalido",
+    < 90   => "angulo agudo",
+      90   => "angulo recto",
+    < 180 => "angulo Obtuso",
+    180 => "angulo llano",
+
+};
+Console.WriteLine($"el angulo de {angulo} grados es un angulo {clasificacion}");
