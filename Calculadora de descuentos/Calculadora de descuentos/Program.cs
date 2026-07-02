1d3fdbf213e456e6189714a13dabe0361355b679
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:05:08 -0400

    Add files via upload

---
+++ Calculadora de descuentos/Calculadora de descuentos/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("ingrese el precio del producto");
+int producto = int.Parse(Console.ReadLine());
+
+string clasificacion = producto switch
+{
+    <50 => "no tiene descuento",
+    >=50 and <100 => "tiene un descuento del 5%",
+    >100 => "tiene un descuento del 10%",
+};
+Console.WriteLine(clasificacion);
---
+++ calculo de impuestos/calculo de impuestos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Concurrent;
+
+Console.WriteLine("ingrese su salario mensual ");
+int salario = int.Parse(Console.ReadLine());
+
+string impuesto = salario switch
+{
+    < 10000 => " usted no paga impuestos 0%",
+    > 10000 and < 30000 => " su impuestos a pagar son del 10%",
+    > 30000 => "sus impuestos a pagar son del 20%",
+};
+Console.WriteLine(impuesto);
