13437abec48d73885ca47c46a9b98bd6b4102471
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:19:12 -0400

    Add files via upload

---
+++ Verificación de año bisiesto/Verificación de año bisiesto/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Ingresa un año: ");
+int año = int.Parse(Console.ReadLine());
+
+if (año % 400 == 0)
+{
+    Console.WriteLine($"{año} es bisiesto");
+}
+else if (año % 100 == 0)
+{
+    Console.WriteLine($"{año} no es bisiesto");
+}
+else if (año % 4 == 0)
+{
+    Console.WriteLine($"{año} es bisiesto");
+}
+else
+{
+    Console.WriteLine($"{año} no es bisiesto");
+}
