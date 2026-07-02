e97550381b31e3b1367386b3b7881d62cb5478d4
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:09:52 -0400

    Add files via upload

---
+++ Conversión de calificaciones/Conversión de calificaciones/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ingrese su calificacion");
+int calificacion = int.Parse(Console.ReadLine());
+
+string clasificacion = calificacion switch
+{
+    >= 90 and <= 100 => "A",
+    >= 80 and <= 89 => "B",
+    >= 70 and <= 79 => "C",
+    >= 60 and <= 69 => "D",
+    >= 0 and <= 59 => "F",
+};
+Console.WriteLine("Su calificacion es: " + clasificacion );
