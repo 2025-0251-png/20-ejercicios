8f64255c68104efd2c1a7c9ff03915283fdf7ea5
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:12:08 -0400

    Add files via upload

---
+++ descuento por edad/descuento por edad/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("ingrese su nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("ingrese su edad");
+int edad = int.Parse(Console.ReadLine());
+
+if (edad >= 60)
+{
+    Console.WriteLine($"señor/a {nombre} usted tiene un descuento del 50% en su entrada");
+}
+else if (edad < 60)
+{
+    Console.WriteLine($"señor/a {nombre} usted no aplica para el descuento");
+}
