2ec0cdd66ccb032ed09578a0ce094b8d0dbfde1c
2025-0251-png <2025-0251@unad.edu.do>
2026-07-02 01:15:06 -0400

    Add files via upload

---
+++ triangulo/triangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Lado a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Lado b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Lado c: ");
+double c = double.Parse(Console.ReadLine());
+
+if (a == b & b == c)
+{
+    Console.WriteLine("El triángulo es Equilátero");
+}
+else if (a == b || b == c || a == c)
+{
+    Console.WriteLine("El triángulo es Isósceles");
+}
+else
+{
+    Console.WriteLine("El triángulo es Escaleno");
+}
